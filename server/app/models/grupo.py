a27b1b559d68334d513a332950e4487743cbc307
Henrique <henriquepassos2004@outlook.com.br>
2026-06-06 15:48:10 -0300

    Doc: Descrição do modelo da tabela grupo

--- server/app/models/grupo.py
+++ server/app/models/grupo.py
@@ -3,6 +3,15 @@ from sqlalchemy.orm import Mapped, mapped_column
 from app.database import Base
 
 class GrupoModel(Base):
+    """
+    Representa a tabela 'grupo' no banco de dados.
+    
+    Campos obrigatórios:
+        nome_grupo, data_craicao
+        
+    Campos opcionais/estrangeiros:
+        descricao
+    """
     __tablename__ = "grupo"
 
     id_grupo: Mapped[int] = mapped_column("ID_GRUPO", Integer, primary_key=True, autoincrement=True)
